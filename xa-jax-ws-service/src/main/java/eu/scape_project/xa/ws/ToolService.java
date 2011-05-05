b21a5fecbd465438132471073dc0cc963eb27109
Andrew Jackson <Andrew.Jackson@bl.uk>
2011-05-05 16:29:57 +0100

    Added experimental JAX-WS service endpoint implementation.

---
+++ xa-jax-ws-service/src/main/java/eu/scape_project/xa/ws/ToolService.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package eu.scape_project.xa.ws;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.util.List;
+import java.util.Vector;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.xml.ws.RequestWrapper;
+
+import eu.planets_project.services.identify.IdentifyResult;
+import eu.planets_project.services.identify.IdentifyResult.Method;
+import eu.scape_project.pit.invoke.CommandNotFoundException;
+import eu.scape_project.pit.invoke.PitInvoker;
+import eu.scape_project.pit.invoke.ToolSpecNotFoundException;
+import eu.scape_project.pit.tools.Parameters;
+
+/**
+ * http://fue.onb.ac.at/axis2-l/services/IMPACTOpenjpegConversionService?wsdl
+ * 
+ * @author anj
+ *
+ */
+@WebService
+//SOAPBinding(style=SOAPBinding.Style.DOCUMENT, use=SOAPBinding.Use.ENCODED, parameterStyle = SOAPBinding.ParameterStyle.WRAPPED)
+public class ToolService {
+
+	private PitInvoker ib;
+
+	public ToolService() {
+		try {
+			ib  = new PitInvoker("file");
+		} catch (ToolSpecNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	// 
+	@WebMethod(operationName="identify")
+	@WebResult(name="identifyResult")
+	@RequestWrapper(localName="arguments")
+	public IdentifyResult identify( 
+			@WebParam(name="toolId") String toolId, 
+			@WebParam(name="inputURI") URI input, 
+			@WebParam(name="parameters") Parameters parameters ) {
+		try {
+			ib.identify(toolId, new File( input ) );
+		} catch (CommandNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		List<URI> formats = new Vector<URI>();
+		formats.add(URI.create("image/jpeg"));
+		return new IdentifyResult( formats, Method.MAGIC, null );
+	}
+	
+}
--- xa-pit/src/main/java/eu/scape_project/pit/invoke/PitInvoker.java
+++ xa-pit/src/main/java/eu/scape_project/pit/invoke/PitInvoker.java
@@ -40,37 +40,17 @@ public class PitInvoker {
 		}
 		
 	}
-
-	public void invoke( String command_id, File input, File output) throws CommandNotFoundException, IOException {
+	
+	private Tool findTool( String command_id ) throws CommandNotFoundException {
 		Tool cmd = null;
 		for( Tool c : ts.getTools() ) {
 			if( c.getId() != null && c.getId().equals(command_id) ) cmd = c;
 		}
 		if( cmd == null ) throw new CommandNotFoundException("No command "+command_id+" could be found.");
-
-		// Substitute the templates into the command string:
-		HashMap<String,String> tpls = new HashMap<String,String>();
-		for( Template v : ts.getTemplate() ) {
-			tpls.put(v.getName(), v.getValue());
-		}
-		String[] cmd_template = cmd.getCommand().split(" ");
-		replaceAll(cmd_template,tpls);
-		
-		// Now substiture the parameters into the templates.
-		HashMap<String,String> vars = new HashMap<String,String>();
-		for( Parameter v : ts.getParam() ) {
-			vars.put(v.getVar(), v.getDefaultValue());
-		}
-		
-		// TODO Check input file exists, and output file does not!
-		// Create standard parameters.
-		vars.put("inFile", input.getAbsolutePath());
-		vars.put("outFile", output.getAbsolutePath());
-		vars.put("logFile", File.createTempFile(ts.getName()+"-"+command_id, ".log").getAbsolutePath());
-
-		// Now substitute the parameters:
-		replaceAll(cmd_template,vars);
-
+		return cmd;
+	}
+	
+	private void runCommand( String[] cmd_template ) throws IOException {
 		// Build the command:
 		ProcessBuilder pb = new ProcessBuilder(cmd_template);
 		System.out.println("Command : "+pb.command());
@@ -90,13 +70,71 @@ public class PitInvoker {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	public String[] substituteTemplates( Tool cmd ) {
+		String[] cmd_template = cmd.getCommand().split(" ");
+		if( ts.getTemplate() == null ) return cmd_template;
+		// Substitute the templates into the command string:
+		HashMap<String,String> tpls = new HashMap<String,String>();
+		for( Template v : ts.getTemplate() ) {
+			tpls.put(v.getName(), v.getValue());
+		}
+		replaceAll(cmd_template,tpls);
+		return cmd_template;
+	}
+	
+	public HashMap<String,String> getStandardVars( Tool cmd, File input ) throws IOException {
+		// Now substiture the parameters into the templates.
+		HashMap<String,String> vars = new HashMap<String,String>();
+		if( ts.getParam() != null ) {
+			for( Parameter v : ts.getParam() ) {
+				vars.put(v.getVar(), v.getDefaultValue());
+			}
+		}
+		// TODO Check input file exists!
+		// Create standard parameters.
+		vars.put("inFile", input.getAbsolutePath());
+		vars.put("logFile", File.createTempFile(ts.getName()+"-"+cmd.getId(), ".log").getAbsolutePath());
+		return vars;
+	}
+
+	public void identify( String command_id, File input ) throws CommandNotFoundException, IOException {
+		Tool cmd = findTool(command_id);
+		String[] cmd_template = substituteTemplates(cmd);
+		HashMap<String, String> vars = getStandardVars(cmd, input);
+		
+		for( String key : vars.keySet() ) {
+			System.out.println("Key: "+key+" = "+vars.get(key));
+		}
+		
+		// Now substitute the parameters:
+		replaceAll(cmd_template,vars);
+
+		// Now run the command:
+		runCommand(cmd_template);
+	}
+	
+	public void convert( String command_id, File input, File output) throws CommandNotFoundException, IOException {
+		Tool cmd = findTool(command_id);
+		String[] cmd_template = substituteTemplates(cmd);
+		HashMap<String, String> vars = getStandardVars(cmd, input);
+		
+		// TODO Check output file does not exist!
+		vars.put("outFile", output.getAbsolutePath());
+
+		// Now substitute the parameters:
+		replaceAll(cmd_template,vars);
+
+		// Now run the command:
+		runCommand(cmd_template);
 		
 	}
 	
 	private void replaceAll(String[] cmd_template, HashMap<String,String> vars) {
 		for( int i = 0; i < cmd_template.length; i++ ) {
 			for( String key : vars.keySet() ) {
-				String matchTo = Pattern.quote("%{"+key+"}");
+				String matchTo = Pattern.quote("${"+key+"}");
 				cmd_template[i] = cmd_template[i].replaceAll(matchTo, vars.get(key).replace("\\", "\\\\") );
 			}
 		}
@@ -110,10 +148,11 @@ public class PitInvoker {
 	 * @throws CommandNotFoundException 
 	 */
 	public static void main(String[] args) throws IOException, ToolSpecNotFoundException, CommandNotFoundException {
-		PitInvoker ib = new PitInvoker("jasper");
-		ib.invoke("minimally-lossless", 
-				new File("test.jpeg"), 
-				new File("test.jp2") );
+		PitInvoker ib = new PitInvoker("file");
+		ib.identify("file-id-mime", 
+				new File("test.jpeg") );
+				//, 
+				//new File("test.jp2") );
 //				File.createTempFile("DISC_1",".iso") );
 	}
 	
