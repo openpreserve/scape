424e0d70ac5ce993b8b15b6913d2d383d3933456
Andrew Jackson <Andrew.Jackson@bl.uk>
2011-05-11 13:21:56 +0100

    Switched to better version of JAX-WS

--- xa-jax-ws-service/src/main/java/eu/scape_project/xa/ws/ToolService.java
+++ xa-jax-ws-service/src/main/java/eu/scape_project/xa/ws/ToolService.java
@@ -9,16 +9,20 @@ import java.net.URI;
 import java.util.List;
 import java.util.Vector;
 
+import javax.activation.DataHandler;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
+import javax.xml.bind.annotation.XmlMimeType;
 import javax.xml.ws.RequestWrapper;
 import javax.xml.ws.ResponseWrapper;
+import javax.xml.ws.WebServiceException;
 import javax.xml.ws.soap.MTOM;
 
 import com.sun.xml.ws.developer.StreamingAttachment;
+import com.sun.xml.ws.developer.StreamingDataHandler;
 
 import eu.planets_project.services.identify.IdentifyResult;
 import eu.planets_project.services.identify.IdentifyResult.Method;
@@ -33,8 +37,8 @@ import eu.scape_project.pit.tools.Parameters;
  * @author anj
  *
  */
-@MTOM
-@StreamingAttachment(parseEagerly=true, memoryThreshold=4000000L)
+//MTOM
+//StreamingAttachment(parseEagerly=true, memoryThreshold=4000000L)
 @WebService
 //SOAPBinding(style=SOAPBinding.Style.DOCUMENT, use=SOAPBinding.Use.ENCODED, parameterStyle = SOAPBinding.ParameterStyle.WRAPPED)
 public class ToolService {
@@ -53,6 +57,7 @@ public class ToolService {
 	// 
 	@WebMethod(operationName="identify")
 	@WebResult(name="identifyResult")
+	@RequestWrapper(localName="arguments")
 	//@RequestWrapper(partName="arguments")
 	//@ResponseWrapper(partName="results")
 	public IdentifyResult identify( 
@@ -62,15 +67,24 @@ public class ToolService {
 		try {
 			ib.identify(toolId, new File( input ) );
 		} catch (CommandNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+            throw new WebServiceException(e);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+            throw new WebServiceException(e);
 		}
 		List<URI> formats = new Vector<URI>();
 		formats.add(URI.create("image/jpeg"));
 		return new IdentifyResult( formats, Method.MAGIC, null );
 	}
 	
+	public void fileUpload(String name, @XmlMimeType("application/octet-stream") DataHandler data) {
+        try {
+             StreamingDataHandler dh = (StreamingDataHandler)data;
+             System.out.println("Got DataHander: "+dh);
+             File file = File.createTempFile(name, "");
+             dh.moveTo(file);
+             dh.close();
+        } catch(Exception e) {
+             throw new WebServiceException(e);
+        }
+    }
 }
