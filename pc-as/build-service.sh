3788b88ffda48a54b9fe8b5a5dc40e276c6092ca
Rui Castro <rcastro@keep.pt>
2011-12-13 12:33:26 +0000

    updated script to create services from toolspecs and imagemagick and mencoder toolspecs

--- pc-as/build-service.sh
+++ pc-as/build-service.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+if test -z "$1"
+then
+	echo "ERROR: Service definition file not specified"
+	echo "USAGE: $0 service-definition-xml"
+	exit 1
+fi
+
 scriptdir=`dirname "$0"`
 # Get absolute path for $scriptdir
 scriptdir=`readlink -m $scriptdir`
@@ -8,6 +15,11 @@ scriptdir=`readlink -m $scriptdir`
 xc=`readlink -m $1`
 
 cd $scriptdir/../xa-toolwrapper
+
+# delete all previous generated services
+rm -rf generated/*
+
+# generate the new service(s) project(s)
 java -jar toolwrapper.jar -pc toolwrapper.properties -xc $xc
 
 cd generated
