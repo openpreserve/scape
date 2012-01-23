caf3efae93b757c59e2aab78ef64202c7f03ed74
Hélder Silva <hsilva@keep.pt>
2012-01-23 16:07:36 +0000

    script to generate all debian packages now can receive an argument to specify the patern of toolspec .xml files to be generated; added java web app to list all web services available and a quick way to execute one through its REST interface by providing the web service and file url's

--- pc-as/generateAllDebianPackages.sh
+++ pc-as/generateAllDebianPackages.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-for i in $(ls scape-as-*)
+if [ $# -eq 1 ]; then
+	SERVICE_PATTERN="$1"
+else
+	SERVICE_PATTERN="scape-as-*"
+fi
+
+for i in $(ls $SERVICE_PATTERN)
 do 
 	./build-service.sh $i
 done
 
-for i in $(ls scape-as-*)
+for i in $(ls $SERVICE_PATTERN)
 do 
 	WEB_SERVICE_NAME=`echo $i | sed -e 's/^scape-as-//' -e 's/\.xml$//' -e 's/-//g'`
 	./generateDebianPackage.sh services/*$WEB_SERVICE_NAME*.war $i
