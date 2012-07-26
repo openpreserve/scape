28d51bb9f5c8719f5087866b44f4065a54f5dd45
Hélder Silva <hsilva@keep.pt>
2012-07-26 18:12:22 +0100

    debian packages update (copyrights updated to refer scape)

--- pc-as/debians/digital-preservation-tools-migration/digital-preservation-tools-migration_1.0.0/generateDependsString.sh
+++ pc-as/debians/digital-preservation-tools-migration/digital-preservation-tools-migration_1.0.0/generateDependsString.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+################################################################################
+#                  Copyright 2012 The SCAPE Project Consortium
+#
+#   This software is copyrighted by the SCAPE Project Consortium. 
+#   The SCAPE project is co-funded by the European Union under
+#   FP7 ICT-2009.4.1 (Grant Agreement number 270137).
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#                   http://www.apache.org/licenses/LICENSE-2.0              
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+################################################################################
+
 if [ $# -ne 1 ]; then
 	echo "Must provide the directory containing the dependencies for the meta-package"
 	exit 1
--- pc-as/debians/sanselan/sanselan-wrapper_0.97/sanselan-wrapper/src/main/java/eu/scape_project/sanselan_wrapper/SanselanCli.java
+++ pc-as/debians/sanselan/sanselan-wrapper_0.97/sanselan-wrapper/src/main/java/eu/scape_project/sanselan_wrapper/SanselanCli.java
@@ -7,6 +7,28 @@ import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.PosixParser;
 
+/*
+ ################################################################################
+ #                  Copyright 2012 The SCAPE Project Consortium
+ #
+ #   This software is copyrighted by the SCAPE Project Consortium. 
+ #   The SCAPE project is co-funded by the European Union under
+ #   FP7 ICT-2009.4.1 (Grant Agreement number 270137).
+ #
+ #   Licensed under the Apache License, Version 2.0 (the "License");
+ #   you may not use this file except in compliance with the License.
+ #   You may obtain a copy of the License at
+ #
+ #                   http://www.apache.org/licenses/LICENSE-2.0              
+ #
+ #   Unless required by applicable law or agreed to in writing, software
+ #   distributed under the License is distributed on an "AS IS" BASIS,
+ #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   
+ #   See the License for the specific language governing permissions and
+ #   limitations under the License.
+ ################################################################################
+ */
+
 public class SanselanCli {
 	public static void main(String[] args) {
 		try {
@@ -17,10 +39,12 @@ public class SanselanCli {
 			CommandLineParser parser = new PosixParser();
 			CommandLine cmd = parser.parse(options, args);
 			if (cmd.hasOption("i") && cmd.hasOption("o")) {
-				BufferedImage inImage = SanselanWrapper.load(cmd.getOptionValue("i"), null);
+				BufferedImage inImage = SanselanWrapper.load(
+						cmd.getOptionValue("i"), null);
 				SanselanWrapper.convert(inImage, cmd.getOptionValue("o"), null);
 			} else {
-				System.out.println("usage: (-i|--input-file=) INPUT_FILE (-o|--output-file=) OUTPUT_FILE");
+				System.out
+						.println("usage: (-i|--input-file=) INPUT_FILE (-o|--output-file=) OUTPUT_FILE");
 				System.exit(1);
 			}
 		} catch (Exception e) {
